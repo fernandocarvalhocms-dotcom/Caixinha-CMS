f256c1ddf9a6f4df3f294daa845582ac70ad3787
fernandocarvalhocms-dotcom <fernando.carvalho.cms@gmail.com>
2025-12-09 12:03:05 -0300

    fix: Corrigir variáveis Vite em import.meta.env

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,8 @@ export default defineConfig(({ mode }) => {
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-           'process.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
-           'process.env.VITE_SUPABASE_KEY': JSON.stringify(env.VITE_SUPABASE_KEY),
+           'pimport.meta.env.VITE_SUPABASE_URL'
+           'import.meta.env.VITE_SUPABASE_KEY'
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
